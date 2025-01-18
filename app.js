fb2987af324ceed8e1c822ad1af76fb5418873e6
manichandra-donthula <mrmanichandra222@gmail.com>
2025-01-18 19:29:00 +0530

    Initial Setup and DB Connection

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Server is running! This is Home Page!!");
+});
+
+// For unmatched routes
+app.use((req, res, next) => {
+  res.status(404).send("Page not found. Please check the URL.");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port http://localhost:${process.env.PORT}`);
+});
